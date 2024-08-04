9e2ead75a041984b08a65da3e8c15be3e65921d0
Josh Harris <jdharrisnz@icloud.com>
2024-08-04 21:58:14 +0100

    Add preprocessed files

---
+++ src/output/transitionTimingConfig.ts
@@ -0,0 +1,6 @@
+export const transitionTimingConfig = {
+  "transitionTimingFunction": {
+    "bp5": "cubic-bezier(0.4, 1, 0.75, 0.9)",
+    "bp5-bounce": "cubic-bezier(0.54, 1.12, 0.38, 1.11)"
+  }
+}
