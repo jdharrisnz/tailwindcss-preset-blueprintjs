c81d683c9eb5eb192b10c6a545deb51e54f552e1
Josh Harris <jdharrisnz@icloud.com>
2024-08-04 21:49:22 +0100

    80 line width

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,12 @@
 /** @type {import("prettier").Config} */
 
 const config = {
-    plugins: ["prettier-plugin-jsdoc", "prettier-plugin-tailwindcss"],
-    printWidth: 100,
-    proseWrap: "always",
-    semi: false,
-    tabWidth: 2,
-    trailingComma: "none"
+  plugins: ["prettier-plugin-jsdoc", "prettier-plugin-tailwindcss"],
+  printWidth: 80,
+  proseWrap: "always",
+  semi: false,
+  tabWidth: 2,
+  trailingComma: "none"
 }
 
 export default config
