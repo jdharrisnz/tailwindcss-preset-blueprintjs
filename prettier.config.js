c4ff88a8db07aba798e4431073cead9a0bd8809e
Josh Harris <jdharrisnz@icloud.com>
2025-02-07 09:46:04 +1300

    Add prettier-plugin-sort-imports

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,36 @@
 /** @type {import("prettier").Config} */
-
-const config = {
-  plugins: ["prettier-plugin-jsdoc", "prettier-plugin-tailwindcss"],
+export default {
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-jsdoc",
+    "prettier-plugin-tailwindcss",
+  ],
+  experimentalTernaries: true,
   printWidth: 80,
   proseWrap: "always",
   semi: false,
+  singleAttributePerLine: true,
   tabWidth: 2,
-  trailingComma: "none"
-}
+  trailingComma: "all",
 
-export default config
+  // Import sort options
+  importOrder: [
+    "",
+    "^react$", // React
+    "<BUILTIN_MODULES>", // Node.js built-in modules
+    "<THIRD_PARTY_MODULES>", // Imports not matched by other special words or groups
+    "",
+    "^@/", // Local imports via path aliases
+    "^[.]", // Relative imports
+    "",
+    "<TYPES>^(node:)", // Types from node
+    "<TYPES>", // Third-party types
+    "",
+    "<TYPES>^@/", // Local types via path aliases
+    "<TYPES>^[.]", // Relative types
+    "",
+    ".module.(c|sc|le)ss$", // CSS modules
+  ],
+  importOrderCaseSensitive: true,
+  importOrderTypeScriptVersion: "5.0.0",
+}
