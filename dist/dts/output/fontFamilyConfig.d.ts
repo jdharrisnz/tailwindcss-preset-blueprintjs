ed10392eac5776eb653ba8df3c42451651d5c914
Josh Harris <jdharrisnz@icloud.com>
2025-02-07 09:44:23 +1300

    Don't track dist folder

--- dist/dts/output/fontFamilyConfig.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const fontFamilyConfig: {
-    fontFamily: {
-        "bp5-sans": string[];
-        "bp5-mono": string[];
-    };
-};
-//# sourceMappingURL=fontFamilyConfig.d.ts.map
--- dist/dts/output/transitionTimingConfig.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const transitionTimingConfig: {
-    transitionTimingFunction: {
-        bp5: string;
-        "bp5-bounce": string;
-    };
-};
-//# sourceMappingURL=transitionTimingConfig.d.ts.map
